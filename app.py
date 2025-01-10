b01d38e13b29f89e887827593c9e3fccb430fbc2
ady7ady7 <115740608+ady7ady7@users.noreply.github.com>
2025-01-10 17:20:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,23 @@ import scraper
 
 app = Flask(__name__)
 
-# Proste uruchamianie scrapera w osobnym wątku
 def start_scraper():
-    print("[INFO] Uruchamianie scrapera...")
-    thread = threading.Thread(target=scraper.run_scraper, daemon=True)
-    thread.start()
+    print("[INFO] Scraper uruchomiony")
+    threading.Thread(target=scraper.run_scraper, daemon=True).start()
 
-# Uruchamianie scraperów przy starcie aplikacji
+# Uruchom scraper przy starcie aplikacji
 start_scraper()
 
 @app.route("/")
 def index():
-    return render_template("index.html", assets=scraper.asset_list, refresh_minutes=5, current_values=scraper.current_values)
+    return render_template("index.html", assets=scraper.asset_list, current_values=scraper.current_values)
 
 @app.route("/status")
 def status():
     return jsonify({
-        "last_update": scraper.last_request_time,
-        "refresh_interval": 5  # Interwał odświeżania w minutach
+        "last_update": scraper.last_request_time
     })
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
