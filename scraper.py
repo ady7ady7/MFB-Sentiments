50f92a1fc5eccdf2b2f8786619e6208558c7275f
ady7ady7 <115740608+ady7ady7@users.noreply.github.com>
2025-01-10 17:43:46 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,8 +7,8 @@ from datetime import datetime
 asset_list = ["EURUSD", "GBPUSD", "USDJPY", "AUDUSD", "XAUUSD"]
 last_request_time = None
 
-# Aktualne wartości do wyświetlania
-current_values = {asset: {"long": 0, "short": 0} for asset in asset_list}
+# Aktualne wartości do wyświetlania na stronie
+current_values = {asset: {"long": "N/A", "short": "N/A"} for asset in asset_list}
 
 def run_scraper():
     global last_request_time
@@ -16,9 +16,7 @@ def run_scraper():
     while True:
         for asset in asset_list:
             url = f"https://www.myfxbook.com/community/outlook/{asset}"
-            headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-            }
+            headers = {"User-Agent": "Mozilla/5.0"}
             try:
                 response = requests.get(url, headers=headers)
                 response.raise_for_status()
@@ -33,15 +31,15 @@ def run_scraper():
                     cells = row.find_all('td')
                     if len(cells) > 1:
                         if 'Long' in cells[0].text.strip():
-                            new_long = int(cells[1].text.strip().replace('%', ''))
+                            new_long = cells[1].text.strip()
                         elif 'Short' in cells[0].text.strip():
-                            new_short = int(cells[1].text.strip().replace('%', ''))
+                            new_short = cells[1].text.strip()
 
-                if new_long is not None and new_short is not None:
+                if new_long and new_short:
                     current_values[asset] = {"long": new_long, "short": new_short}
-                    print(f"[INFO] {asset} - Long: {new_long}%, Short: {new_short}%")
+                    print(f"[INFO] {asset} - Long: {new_long}, Short: {new_short}")
                 else:
-                    print(f"[WARNING] Nie znaleziono danych dla {asset}")
+                    print(f"[WARNING] Brak danych dla {asset}.")
 
             except requests.exceptions.HTTPError as http_err:
                 print(f"[ERROR] Błąd HTTP dla {asset}: {http_err}")
@@ -49,5 +47,5 @@ def run_scraper():
                 print(f"[ERROR] Inny błąd dla {asset}: {e}")
 
         last_request_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        time.sleep(300)
+        time.sleep(300)  # 5 minut przerwy
 
