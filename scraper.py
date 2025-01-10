93d96a092014fdfd9c53d8d3a108f7fe6e5ac07b
ady7ady7 <115740608+ady7ady7@users.noreply.github.com>
2025-01-10 17:35:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,7 +16,9 @@ def run_scraper():
     while True:
         for asset in asset_list:
             url = f"https://www.myfxbook.com/community/outlook/{asset}"
-            headers = {"User-Agent": "Mozilla/5.0"}
+            headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+            }
             try:
                 response = requests.get(url, headers=headers)
                 response.raise_for_status()
