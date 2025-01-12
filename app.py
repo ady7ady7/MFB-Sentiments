1ecaedaa1c5b9b18688558ab97e20fe2153e0e02
ady7ady7 <115740608+ady7ady7@users.noreply.github.com>
2025-01-12 12:56:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import scraper
 app = Flask(__name__)
 
 def start_all_scrapers():
-    # Uruchamianie scraperów z różnymi opóźnieniami
-    threading.Thread(target=scraper.run_scraper_for_assets, args=(scraper.assets1, 0), daemon=True).start()  # Bez opóźnienia
-    threading.Thread(target=scraper.run_scraper_for_assets, args=(scraper.assets2, 0), daemon=True).start()  # 1.5 minuty opóźnienia
-    threading.Thread(target=scraper.run_scraper_for_assets, args=(scraper.assets3, 0), daemon=True).start()  # 3 minuty opóźnienia
+    """Uruchamianie scraperów dla wszystkich grup."""
+    threading.Thread(target=scraper.run_scraper_for_group, args=(scraper.assets1, "group1"), daemon=True).start()
+    threading.Thread(target=scraper.run_scraper_for_group, args=(scraper.assets2, "group2"), daemon=True).start()
+    threading.Thread(target=scraper.run_scraper_for_group, args=(scraper.assets3, "group3"), daemon=True).start()
 
 start_all_scrapers()
 
@@ -47,17 +47,19 @@ def get_plot_data(asset):
         autosize=True,
         margin=dict(l=20, r=20, t=20, b=20),
         yaxis=dict(range=[-5, 105], tickmode='linear', dtick=10, showgrid=True, zeroline=False
-    ))
+    )
 
     return fig.to_json()
 
 @app.route("/status")
 def status():
+    """Zwraca czas ostatniej aktualizacji każdej grupy."""
     return jsonify({
-        "last_update": scraper.last_request_time
+        "last_update_times": scraper.last_update_times
     })
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Pobieranie portu ze zmiennej środowiskowej lub domyślnie 5000
     app.run(host="0.0.0.0", port=port, debug=False)  # Nasłuch na 0.0.0.0 dla wszystkich adresów sieciowych
+
 
